28efa801fad3aa959a22b854637e439de5043607
Dirk van Krieken <dvankrieken@protonmail.com>
2021-05-01 21:09:27 +0200

    done with %x and %X conversions

--- dev/srcs/putfunctions/putx/pf_putx.c
+++ dev/srcs/putfunctions/putx/pf_putx.c
@@ -1,19 +1,47 @@
 #include "../../../includes/ft_printf.h"
 
-void	pf_putx(t_convert *p, int isupper)
+static	void	pf_putx_width(t_convert *p, int len)
+{
+	int i;
+
+	i = 0;
+	while (i < (p->width - p->precision))
+		{
+			ft_putchar_fd(' ', 1);
+			i++;
+		}
+		while (p->precision > len && i < (p->width - len))
+		{
+			ft_putchar_fd('0', 1);
+			i++;
+		}
+}
+
+void		pf_putx(t_convert *p, int isupper)
 {
 	char *string;
-
 	int len;
+	int i;
+
 	if (isupper)
 		string = pf_ultohex(p->X, isupper);
 	else
 		string = pf_ultohex(p->x, isupper);
 	len = ft_strlen(string);
-	while (p->width > len)
+	i = 0;
+	if (p->width > len)
 	{
-		ft_putchar_fd(' ', 1);
-		len++;
+		pf_putx_width(p, len);
+	}
+	else if (p->precision > len)
+	{
+		i = len;
+		while (p->precision > i)
+		{
+			ft_putchar_fd('0', 1);
+			i++;
+		}
 	}
 	ft_putstr_fd(string, 1);
+
 }
