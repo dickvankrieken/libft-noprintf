ef339d70876d636bb50bb2facafb99b264816d26
Dirk van Krieken <dvankrieken@protonmail.com>
2021-05-17 09:55:44 +0200

    working on %%

--- Makefile
+++ Makefile
@@ -1,38 +1,35 @@
-NAME = libftprintf.a
+NAME =		libftprintf.a
 
-
-
-# DIRECTORIES #
-SRC_DIR =				srcs
-HDR_DIR =				includes
-OBJ_DIR =				obj
-LIBFT_DIR = 			srcs/libft
+#---------------------- DIRECTORIES ------------------#
+SRC_DIR =	srcs
+HDR_DIR =	includes
+OBJ_DIR =	obj
+LIBFT_DIR = 	srcs/libft
 
 #---------------------- SOURCES ----------------------#
-
-SRCS =		 			srcs/ft_printf.c \
-						srcs/pf_checkfunctions.c \
-						srcs/putfunctions/pf_putfunctions.c \
-						srcs/putfunctions/puts/pf_puts.c \
-						srcs/putfunctions/puts/pf_puts_width.c \
-						srcs/putfunctions/putd/pf_putd.c \
-						srcs/putfunctions/putd/pf_putd_minus.c \
-						srcs/putfunctions/putd/pf_putd_zero.c \
-						srcs/putfunctions/putp/pf_putp.c \
-						srcs/putfunctions/putx/pf_putx.c \
-						srcs/pf_ultohex.c \
-						srcs/putfunctions/putu/pf_putu.c \
-						srcs/putfunctions/putu/pf_putu_minus.c \
-						srcs/putfunctions/putu/pf_putu_zero.c \
-						srcs/printspecs.c
+SRCS =		srcs/ft_printf.c \
+		srcs/pf_checkfunctions.c \
+		srcs/putfunctions/pf_putfunctions.c \
+		srcs/putfunctions/puts/pf_puts.c \
+		srcs/putfunctions/puts/pf_puts_width.c \
+		srcs/putfunctions/putd/pf_putd.c \
+		srcs/putfunctions/putd/pf_putd_minus.c \
+		srcs/putfunctions/putd/pf_putd_zero.c \
+		srcs/putfunctions/putp/pf_putp.c \
+		srcs/putfunctions/putx/pf_putx.c \
+		srcs/pf_ultohex.c \
+		srcs/putfunctions/putu/pf_putu.c \
+		srcs/putfunctions/putu/pf_putu_minus.c \
+		srcs/putfunctions/putu/pf_putu_zero.c \
+		srcs/printspecs.c
 
 #---------------------- OBJECTS ----------------------#
-OBJS =		 			$(SRCS:.c=.o)
+OBJS =		$(SRCS:.c=.o)
 
-#---------------------- FLAGS ----------------------#
-C_FLAGS 	= 	-Werror -Wextra -Wall
+#---------------------- FLAGS ------------------------#
+C_FLAGS =	-Werror -Wextra -Wall
 
-#---------------------- RECIPES ----------------------#
+#---------------------- RULES ------------------------#
 all: $(NAME)
 
 test: $(NAME)
--- main.c
+++ main.c
@@ -3,50 +3,50 @@
 #include <stdio.h>
 
 
-static void testd()
-{
+/* static void testd() */
+/* { */
 
-	int i = 2345;
-	int i2 = 2345678;
-	int i3 = 234567890;
+/* 	int i = 2345; */
+/* 	int i2 = 2345678; */
+/* 	int i3 = 234567890; */
 
-	   printf("printf    >>%10.3d<<\n", i);
-	ft_printf("ft_printf >>%10.3d<<\n", i);
-	   printf("printf    >>%10.3d<<\n", i2);
-	ft_printf("ft_printf >>%10.3d<<\n", i2);
-	   printf("printf    >>%10.3d<<\n", i3);
-	ft_printf("ft_printf >>%10.3d<<\n", i3);
-}
+/* 	   printf("printf    >>%10.3d<<\n", i); */
+/* 	ft_printf("ft_printf >>%10.3d<<\n", i); */
+/* 	   printf("printf    >>%10.3d<<\n", i2); */
+/* 	ft_printf("ft_printf >>%10.3d<<\n", i2); */
+/* 	   printf("printf    >>%10.3d<<\n", i3); */
+/* 	ft_printf("ft_printf >>%10.3d<<\n", i3); */
+/* } */
 
-static void testx()
-{
-	int tohex = 190607;
-	int tohex2 = 121990607;
+/* static void testx() */
+/* { */
+/* 	int tohex = 190607; */
+/* 	int tohex2 = 121990607; */
 
-	   printf("printf    >>%3.3x<<< en >>>%.10x<<<\n", tohex, tohex2);
-	ft_printf("ft_printf >>%3.3x<<< en >>>%.10x<<<\n", tohex, tohex2);
-	   printf("printf    >>%3.3x<<< en >>>%.10x<<<\n", tohex, tohex2);
-	ft_printf("ft_printf >>%3.3x<<< en >>>%.10x<<<\n", tohex, tohex2);
-}
+/* 	   printf("printf    >>%3.3x<<< en >>>%.10x<<<\n", tohex, tohex2); */
+/* 	ft_printf("ft_printf >>%3.3x<<< en >>>%.10x<<<\n", tohex, tohex2); */
+/* 	   printf("printf    >>%3.3x<<< en >>>%.10x<<<\n", tohex, tohex2); */
+/* 	ft_printf("ft_printf >>%3.3x<<< en >>>%.10x<<<\n", tohex, tohex2); */
+/* } */
 
-static void testp()
-{
-	char c = 'e';
-	char *helloworld = "hello world";
-	char *pointer = helloworld;
-	char *ppointer = &c;
+/* static void testp() */
+/* { */
+/* 	char c = 'e'; */
+/* 	char *helloworld = "hello world"; */
+/* 	char *pointer = helloworld; */
+/* 	char *ppointer = &c; */
 
-	ft_printf(">>>%13p<<< en >>>%3p<<<\n", pointer, ppointer);
-	printf(">>>%13p<<< en >>>%3p<<<\n", pointer, ppointer);
-}
+/* 	ft_printf(">>>%13p<<< en >>>%3p<<<\n", pointer, ppointer); */
+/* 	printf(">>>%13p<<< en >>>%3p<<<\n", pointer, ppointer); */
+/* } */
 
-static void testu()
-{
-	unsigned int uint1 = 12345;
-	unsigned int uint2 = -1;
-	ft_printf(">>>%u<<< en >>>%u<<<\n", uint1, uint2);
-	printf(">>>%u<<< en >>>%u<<<\n", uint1, uint2);
-}
+/* static void testu() */
+/* { */
+/* 	unsigned int uint1 = 12345; */
+/* 	unsigned int uint2 = -1; */
+/* 	ft_printf(">>>%u<<< en >>>%u<<<\n", uint1, uint2); */
+/* 	printf(">>>%u<<< en >>>%u<<<\n", uint1, uint2); */
+/* } */
 
 
 int main()
@@ -58,7 +58,8 @@ int main()
 	/* testp(); */
 	/* printf("\ntestd:\n"); */
 	/* testd(); */
-	printf("\ntestu:\n");
-	testu();
-
+	/* printf("\ntestu:\n"); */
+	/* testu(); */
+	printf(">%-20c<\n", '&');
+	ft_printf(">%-20c<\n", '&');
 }
