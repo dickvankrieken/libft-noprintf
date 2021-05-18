ba0a41319da7982d89853dc8250e4ab05cdc8800
Dirk van Krieken <dvankrieken@protonmail.com>
2021-05-18 11:24:57 +0200

    %s nu compleet en count

--- srcs/putfunctions/puts/pf_puts.c
+++ srcs/putfunctions/puts/pf_puts.c
@@ -1,14 +1,80 @@
 #include "../../../includes/ft_printf.h"
 
+static void	pf_puts_width_zero(t_convert *p)
+{
+	if (p->precision > -1)
+	{
+		pf_putzero(p->width - p->precision);
+		write(1, p->s, p->precision);
+	}
+	else
+	{
+		pf_putzero(p->width - ft_strlen(p->s));
+		write(1, p->s, ft_strlen(p->s));
+	}
+}
+
+static void	pf_puts_width(t_convert *p)
+{
+	if (p->zero)
+		pf_puts_width_zero(p);
+	else
+	{
+		if (p->precision)
+		{
+			write(1, p->s, p->precision);
+			pf_putspaces(p->width - p->precision);
+		}
+		else
+		{
+			pf_putspaces(p->width - ft_strlen(p->s));
+			write(1, p->s, ft_strlen(p->s));
+		}
+	}
+	p->count += p->width;
+}
+
+static void	pf_puts_minus(t_convert *p)
+{
+	if (p->width)
+	{
+		if (p->precision > -1)
+		{
+			write(1, p->s, p->precision);
+			pf_putspaces(p->width - p->precision);
+		}
+		else
+		{
+			write(1, p->s, ft_strlen(p->s));
+			pf_putspaces(p->width - ft_strlen(p->s));
+		}
+		p->count += p->width;
+	}
+	else if (p->precision > -1 && p->precision < (int)ft_strlen(p->s))
+	{
+		p->count += write(1, p->s, p->precision);
+	}
+	else
+	{
+ 		p->count += write(1, p->s, ft_strlen(p->s));
+	}
+}
+
 static void	pf_puts_checkprecision(t_convert *p)
 {
-	if (p->precision)
+	if (p->precision > -1)
 	{
-		write(1, p->s, p->precision);
+
 		if (p->precision < (int)ft_strlen(p->s))
+		{
+			write(1, p->s, p->precision);
 			p->count += p->precision;
+		}
 		else
+		{
+			write(1, p->s, ft_strlen(p->s));
 			p->count += ft_strlen(p->s);
+		}
 	}
 	else
 	{
--- srcs/putfunctions/puts/pf_puts_width.c
+++ srcs/putfunctions/puts/pf_puts_width.c
@@ -1,6 +1,20 @@
 #include "../../../includes/ft_printf.h"
 
-void	pf_puts_width(t_convert *p)
+static void	pf_puts_width_zero(t_convert *p)
+{
+	if (p->precision > -1)
+	{
+		pf_putzero(p->width - p->precision);
+		write(1, p->s, p->precision);
+	}
+	else
+	{
+		pf_putzero(p->width - ft_strlen(p->s));
+		write(1, p->s, ft_strlen(p->s));
+	}
+}
+
+void		pf_puts_width(t_convert *p)
 {
 	if (p->zero)
 		pf_puts_width_zero(p);
@@ -18,18 +32,4 @@ void	pf_puts_width(t_convert *p)
 		}
 	}
 	p->count += p->width;
-}
-
-void	pf_puts_width_zero(t_convert *p)
-{
-	if (p->precision > -1)
-	{
-		pf_putzero(p->width - p->precision);
-		write(1, p->s, p->precision);
-	}
-	else
-	{
-		pf_putzero(p->width - ft_strlen(p->s));
-		write(1, p->s, ft_strlen(p->s));
-	}
 }
